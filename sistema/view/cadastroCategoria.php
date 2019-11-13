27ff644c8d916b815c7a12e6522b7e2f98cf2356
Leonardo <53884336+xdlleo@users.noreply.github.com>
2019-11-12 23:11:14 -0300

    Add files via upload

--- sistema/view/cadastroCategoria.php
+++ sistema/view/cadastroCategoria.php
@@ -3,97 +3,123 @@
 <head>
 	<meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>SISTEMA GERENTE</title>
+	<title>SISTEMA GERENTE - Controle Categorias</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
 </head>
 <body>
-    <?php require_once '../controller/categoriaController.php';?>
-    <!-- FORMULÁRIO PARA EDITAR DADOS -->
-    <?php if(isset($_GET['editar_registro'])){
-        foreach (listaCategorias() as $categorias){
-            if($categorias->idCategoria == $_GET['editar_registro']){ ?>
-    <h2>Editar Cadastro Categorias</h2>
     <div>
-        <form name="editarCategoria" method="POST" action="../controller/categoriaController.php">
-            <div>
-                <input type="hidden" id="idCategoria" name="idCategoria" value="<?php echo $categorias->idCategoria ?>">
-                <label>Nome: </label>
-                <input type="text" id="nome" name="nome" required="required" autofocus value="<?php echo $categorias->nome ?>">
+        <?php include "header.php" ?>
+        <div class="container">        
+            <div class="menu">
+                <?php include "menu_lateral.php" ?>
             </div>
-            <hr>
-            <div>
-                <input type="submit" id="editar" name="editar" value="Editar">
-                <input type="submit" id="cancelar" name="cancelar" value="Cancelar">
-            </div>
-        </form>
-    </div>
-    <?php
-            }
-        }
-    // EXIBIR A MENSAGEM DE CONFIRMAÇÃO DE EXCLUIR DADOS
-    } elseif (isset($_GET['excluir_registro'])) {
-        foreach (listaCategorias() as $categorias){
-            if($categorias->idCategoria == $_GET['excluir_registro']){ ?>
-    <div>
-        <form name="excluirCategoria" method="POST" action="../controller/categoriaController.php">
-            <div>
-                <input type="hidden" id="idCategoria" name="idCategoria" value="<?php echo $categorias->idCategoria ?>">
-                <h2>Excluir Categoria</h2>
-                <h4>Deseja mesmo excluir a categoria?</h4>
-            </div>
-            <hr>
-            <div>
-                <input type="submit" id="excluir" name="excluir" value="Excluir">
-                <input type="submit" id="cancelar" name="cancelar" value="Cancelar">
-            </div>
-        </form>
-    </div>
-    <?php 
-            }
-        }
-    } else { ?>
-    <!-- FORMULÁRIO PARA INSERIR DADOS -->
-    <div>
-    	<h2>Cadastrar Categorias</h2>
-        <div>
-        	<form name="cadastroCategoria" method="POST" action="../controller/categoriaController.php">
-        		<div>
-        			<label>Nome: </label>
-                    <input type="text" id="nome" name="nome" required="required" autofocus>
-                    <input type="hidden" id="idCategoria" name="idCategoria">
+            <div class="form">
+                <?php require_once '../controller/categoriaController.php';?>
+                <!-- FORMULÁRIO PARA EDITAR DADOS -->
+                <?php if(isset($_GET['editar_registro'])){
+                    foreach (listaCategorias() as $categorias){
+                        if($categorias->idCategoria == $_GET['editar_registro']){ ?>
+                <div class="titulo_form">            
+                    <h2>Editar Cadastro Categorias</h2>
                 </div>
-                <hr>
+                <fieldset>
+                    <div class="form_group">
+                        <form name="editarCategoria" method="POST" action="../controller/categoriaController.php">
+                            <div class="campo col_1">
+                                <input type="hidden" id="idCategoria" name="idCategoria" value="<?php echo $categorias->idCategoria ?>">
+                                <label>Nome: </label>
+                                <input class="col1" type="text" id="nome" name="nome" required="required" autofocus value="<?php echo $categorias->nome ?>">
+                            </div>
+                            <div class="botao">
+                                <input class="botao_principal" type="submit" id="editar" name="editar" value="Editar">
+                                <input class="botao_secundario" type="submit" id="cancelar" name="cancelar" value="Cancelar">
+                            </div>
+                        </form>
+                    </div>
+                </fieldset>
+                <?php
+                        }
+                    }
+                // EXIBIR A MENSAGEM DE CONFIRMAÇÃO DE EXCLUIR DADOS
+                } elseif (isset($_GET['excluir_registro'])) {
+                    foreach (listaCategorias() as $categorias){
+                        if($categorias->idCategoria == $_GET['excluir_registro']){ ?>
                 <div>
-                    <input type="submit" id="salvar" name="salvar" value="Salvar">
-                    <input type="reset" value="Limpar" />
+                    <form name="excluirCategoria" method="POST" action="../controller/categoriaController.php">
+                        <input type="hidden" id="idCategoria" name="idCategoria" value="<?php echo $categorias->idCategoria ?>">
+                        <div class="titulo_form">
+                            <h2>Excluir Categoria</h2>
+                        </div>
+                        <div class="confirmacao_excluir">
+                            <h3>Deseja mesmo excluir a categoria?</h3>
+                        </div>
+                        <div class="form_group">
+                            <div class="campo col_1">
+                                <input type="hidden" id="idCategoria" name="idCategoria" value="<?php echo $categorias->idCategoria ?>">
+                                <label>Nome: </label>
+                                <input class="col1" readonly type="text" id="nome" name="nome" required="required" value="<?php echo $categorias->nome ?>">
+                            </div>
+                        </div>
+                        <div class="botao">
+                            <input class="botao_principal" type="submit" id="excluir" name="excluir" value="Excluir">
+                            <input class="botao_secundario" type="submit" id="cancelar" name="cancelar" value="Cancelar">
+                        </div>
+                    </form>
                 </div>
-        	</form>
+                <?php 
+                        }
+                    }
+                } else { ?>
+                <!-- FORMULÁRIO PARA INSERIR DADOS -->
+                <div>
+                    <div class="titulo_form">
+                        <h2>Categorias</h2>
+                    </div>
+                    <fieldset>
+                        <div class="form_group">
+                        	<form name="cadastroCategoria" method="POST" action="../controller/categoriaController.php">
+                        		<div class="campo col_1">
+                        			<label>Nome: </label>
+                                    <input class="col1" type="text" id="nome" name="nome" required="required" autofocus>
+                                </div>
+                                <div class="botao">
+                                    <input class="botao_principal" type="submit" id="salvar" name="salvar" value="Salvar">
+                                    <input class="botao_secundario" type="reset" value="Limpar" />
+                                </div>
+                        	</form>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <?php if (listaCategorias() != null) {?>
+                            <div class="titulo_table">
+                                <h3>Categorias Cadastradas</h3>
+                            </div>
+                            <div  class="table">
+                                <table>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Nome</th>
+                                        <th colspan="2" width="140">Ação</th>
+                                    </tr>
+                                    <?php foreach (listaCategorias() as $categorias){?>
+                                    <tr data-id="<?= $categorias->idCategoria;?>">
+                                        <td><?= $categorias->idCategoria;?></td>
+                                        <td><?= $categorias->nome;?></td>
+                                        <td class="botao_acao"><a href="cadastroCategoria.php?editar_registro=<?php echo $categorias->idCategoria; ?>"><button class="botao_acao_p">Editar</button></a>
+                                            <a href="cadastroCategoria.php?excluir_registro=<?php echo $categorias->idCategoria; ?>"><button class="botao_acao_s">Excluir</button></a></td>
+                                    </tr>
+                                    <?php }?>
+                                </table>
+                            </div>
+                            <?php }?>
+                        </div>
+                    </fieldset>
+                </div>
+                <?php }?>    
+            </div>
         </div>
-        <hr>
-        <div>
-            <?php if (listaCategorias() != null) {?>
-            <h3>CATEGORIAS CADASTRADAS</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nome</th>
-                        <th colspan="2">Ação</th>
-                    </tr>
-                </thead>
-                <?php foreach (listaCategorias() as $categorias){?>
-                <tbody>
-                    <tr data-id="<?= $categorias->idCategoria;?>">
-                        <td><?= $categorias->idCategoria;?></td>
-                        <td><?= $categorias->nome;?></td>
-                        <td><a href="cadastroCategoria.php?editar_registro=<?php echo $categorias->idCategoria; ?>"><button>Editar</button></a></td>
-                        <td><a href="cadastroCategoria.php?excluir_registro=<?php echo $categorias->idCategoria; ?>"><button>Excluir</button></a></td>
-                    </tr>
-                </tbody>
-                <?php }?>
-            </table>
-            <?php }?>
-        </div>
+        <?php include "footer.php" ?>
     </div>
-    <?php }?>    
 </body>
 </html>
