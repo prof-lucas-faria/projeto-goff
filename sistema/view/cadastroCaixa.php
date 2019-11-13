3169aac136d2dc347546d4c4b3fd72647a6893e4
Marcos <marcos_arriel@hotmail.com>
2019-11-12 21:51:50 -0300

    cadastroCaixa.php

--- sistema/view/cadastroCaixa.php
+++ sistema/view/cadastroCaixa.php
@@ -3,93 +3,118 @@
 <head>
 	<meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>SISTEMA GERENTE</title>
+	<title> SISTEMA GERENTE - Controle Caixas </title>
     <link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
 </head>
 <body>
-
-        <?php require_once '../controller/caixaController.php';?>
-        <!-- EXIBIR A MENSAGEM DE CONFIRMAÇÃO DE EXCLUIR DADOS -->
-        <?php if (isset($_GET['excluir_registro'])) {
-            foreach (listacaixas() as $caixas){
-                if($caixas->idCaixa == $_GET['excluir_registro']){ ?>
-        <div>
-            <form name="excluircaixa" method="POST" action="../controller/caixaController.php">
+    <div>
+        <?php include "header.php" ?>
+        <div class="container">
+            <div class="menu">
+                <?php include "menu_lateral.php" ?>
+            </div>
+            <div class="form">
+                <?php require_once '../controller/caixaController.php';?>
+                <!-- EXIBIR A MENSAGEM DE CONFIRMAÇÃO DE EXCLUIR DADOS -->
+                <?php if (isset($_GET['excluir_registro'])) {
+                    foreach (listacaixas() as $caixas){
+                        if($caixas->idCaixa == $_GET['excluir_registro']){ ?>
                 <div>
-                    <input type="hidden" id="idcaixa" name="idcaixa" value="<?php echo $caixas->idcaixa ?>">
-                    <h2>Excluir caixa</h2>
-                    <h4>Deseja mesmo excluir o caixa?</h4>
+                    <form name="excluircaixa" method="POST" action="../controller/caixaController.php">
+                        <div>
+                            <input type="hidden" id="idcaixa" name="idcaixa" value="<?php echo $caixas->idcaixa ?>">
+                            <h2>Excluir caixa</h2>
+                            <h4>Deseja mesmo excluir o caixa?</h4>
+                        </div>
+                        <div>
+                            <div>
+                                <label>Nome: </label>
+                                <input type="text" id="nome" name="nome" required="required" autofocus value="<?php echo $caixas->nome ?>">
+                            </div>
+                            <div>
+                                <label>Funcionário: </label>
+                                <input type="text" id="nome" name="nome" required="required" value="<?php echo $caixas->idFuncionario ?>">
+                            </div>
+                        </div>
+                        
+                        <div>
+                            <input type="submit" id="excluir" name="excluir" value="Excluir">
+                            <input type="submit" id="cancelar" name="cancelar" value="Cancelar">
+                        </div>
+                    </form>
                 </div>
-                <hr>
+                <?php 
+                        }
+                    }
+                } else { ?>
+                <!-- FORMULÁRIO PARA INSERIR DADOS -->
                 <div>
-                    <input type="submit" id="excluir" name="excluir" value="Excluir">
-                    <input type="submit" id="cancelar" name="cancelar" value="Cancelar">
-                </div>
-            </form>
-        </div>
-        <?php 
-                }
-            }
-        } else { ?>
-        <!-- FORMULÁRIO PARA INSERIR DADOS -->
-        <div>
-        	<h2>Cadastrar caixas</h2>
-            <div>
-            	<form name="cadastrocaixa" method="POST" action="../controller/caixaController.php">
-            		<div>
-            			<label>Nome: </label>
-                        <input type="text" id="nome" name="nome" required="required" autofocus>
-                        <input type="hidden" id="idcaixa" name="idcaixa">
+                    <div class="titulo_form">
+                	   <h2>Caixas</h2>
                     </div>
-                    <div>
-                        <label>Funcionário responsável:</label>
-                        <select id="idFuncionario" name="idFuncionario">
-                            <?php foreach (listaFuncionarios() as $funcionarios){?>
-                            <option value="">--Selecione--</option>
-                            <option id="<?= $funcionarios->idFuncionario; ?>" value="<?= $funcionarios->idFuncionario; ?>"><?= $funcionarios->nome;?></option>
+                    <fieldset>
+                        <div class="form_group">
+                        	<form name="cadastrocaixa" method="POST" action="../controller/caixaController.php">
+                        		<div class="campo col_1">
+                        			<label>Nome: </label>
+                                    <input class="col1" type="text" id="nome" name="nome" required="required" autofocus>
+                                </div>
+                                <div class="campo campo2col">
+                                    <div class="campo col_2">
+                                        <label>Funcionário responsável:</label>
+                                        <select class="col2" id="idFuncionario" name="idFuncionario">
+                                            <?php foreach (listaFuncionarios() as $funcionarios){?>
+                                            <option value="">--Selecione--</option>
+                                            <option id="<?= $funcionarios->idFuncionario; ?>" value="<?= $funcionarios->idFuncionario; ?>"><?= $funcionarios->nome;?></option>
+                                            <?php }?>
+                                        </select>
+                                    </div>
+                                    <div class="campo col_2">
+                                        <label>Caixa Inicial: </label>
+                                        <input class="col2" type="text" id="saldoInicial" name="saldoInicial" required="required">
+                                    </div>
+                                </div>
+                                <div class="botao">
+                                    <input class="botao_principal" type="submit" id="salvar" name="salvar" value="Salvar">
+                                    <input class="botao_secundario" type="reset" value="Limpar" />
+                                </div>
+                        	</form>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <?php if (listacaixas() != null) {?>
+                            <div class="titulo_table">
+                                <h3>Caixas Cadastrados</h3>
+                            </div>
+                            <div class="table">
+                                <table>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Nome</th>
+                                        <th>Funcionário Responsável</th>
+                                        <th>Saldo Inicial</th>
+                                        <th width="70">Ação</th>
+                                    </tr>
+                                    <?php foreach (listacaixas() as $caixas){?>
+                                    <tr data-id="<?= $caixas->idCaixa;?>">
+                                        <td><?= $caixas->idCaixa;?></td>
+                                        <td><?= $caixas->nome;?></td>
+                                        <td><?= $caixas->idFuncionario;?></td>
+                                        <td><?= $caixas->saldoInicial;?></td>
+                                        <td width="70"><a href="cadastroCaixa.php?excluir_registro=<?php echo $caixas->idCaixa; ?>"><button class="botao_acao_s">Excluir</button></a></td>
+                                    </tr>
+                                    <?php }?>
+                                </table>
+                            </div>
                             <?php }?>
-                        </select>
-                    </div>
-                    <div>
-                        <label>Caixa Inicial: </label>
-                        <input type="text" id="saldoInicial" name="saldoInicial" required="required">
-                    </div>
-                    <hr>
-                    <div>
-                        <input type="submit" id="salvar" name="salvar" value="Salvar">
-                        <input type="reset" value="Limpar" />
-                    </div>
-            	</form>
+                        </div>
+                    </fieldset>
+                </div>
+                <?php }?>  
             </div>
-            <hr>
-            <div>
-                <?php if (listacaixas() != null) {?>
-                <h3>caixaS CADASTRADAS</h3>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Nome</th>
-                            <th>Funcionário Responsável</th>
-                            <th>Saldo Inicial</th>
-                            <th>Ação</th>
-                        </tr>
-                    </thead>
-                    <?php foreach (listacaixas() as $caixas){?>
-                    <tbody>
-                        <tr data-id="<?= $caixas->idCaixa;?>">
-                            <td><?= $caixas->idCaixa;?></td>
-                            <td><?= $caixas->nome;?></td>
-                            <td><?= $caixas->idFuncionario;?></td>
-                            <td><?= $caixas->saldoInicial;?></td>
-                            <td><a href="cadastroCaixa.php?excluir_registro=<?php echo $caixas->idCaixa; ?>"><button>Excluir</button></a></td>
-                        </tr>
-                    </tbody>
-                    <?php }?>
-                </table>
-                <?php }?>
-            </div>
-        </div>
-        <?php }?>    
+        </div>  
+        <?php include "footer.php" ?>
+    </div>
 </body>
 </html>
